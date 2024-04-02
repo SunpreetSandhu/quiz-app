c95d4d0072d2f356417496789f469b6a587e130c
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-02 17:23:56 -0400

    fetched data from api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import DateCounter from "./DateCounter";
 import "./index.css";
 import Header from "./Header";
 import Main from "./Main";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("error"));
+  }, []);
   return (
     <div className="app">
       <Header />
