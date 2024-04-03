3a5dbd36393678fa8a5d2ca58ad5d0a214489102
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-03 11:02:31 -0400

    created reducer function with ready state dispatched when data is fetched

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import DateCounter from "./DateCounter";
 import "./index.css";
 import Header from "./Header";
 import Main from "./Main";
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  //status; loading, error, ready, active, finished
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    default:
+      throw new Error("action unknown");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(function () {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
       .catch((err) => console.error("error"));
   }, []);
   return (
